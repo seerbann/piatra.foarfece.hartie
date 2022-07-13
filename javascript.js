1f99484c60ba7fe5d708e0aa76624c3f495d7e34
seerbann <100155210+seerbann@users.noreply.github.com>
2022-07-13 15:03:31 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function ComputerPlay(max) {
+  return Math.floor(Math.random() * max);
+}
+let hisSelection=ComputerPlay(3);
+if(hisSelection==0)
+  computerSelection="rock";
+if(hisSelection==1)
+  computerSelection="paper";
+if(hisSelection==2)
+  computerSelection="scissors";
+
+    function playRound(playerSelection, computerSelection) {
+        if(playerSelection=="rock" && computerSelection=="paper")
+        return ("PLAYER WON.ROCK BEATS PAPER");
+        else return ("PLAYER LOST");
+    }
+    let playerSelection="rock";
+
+
+  
